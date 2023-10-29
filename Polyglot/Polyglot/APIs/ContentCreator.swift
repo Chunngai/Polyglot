f8fa4403232b794f8fdd68dbd0f72f7d2c5b064b
Chunngai <mikumikku3939@icloud.com>
2023-10-29 20:58:55 +0700

    update prompts for content creation

--- Polyglot/Polyglot/APIs/ContentCreator.swift
+++ Polyglot/Polyglot/APIs/ContentCreator.swift
@@ -20,13 +20,13 @@ struct ContentCreator {
         let words = words.joined(separator: "\n")
         
         switch self.lang {
-        case LangCode.zh: return "请写一个包含以下单词的段落，注意不要改变所提供单词的形式。\n\(words)"
-        case LangCode.en: return "Please write a paragraph containing the following words, taking care not to change the form of the words provided.\n\(words)"
-        case LangCode.ja: return "次の言葉を含む段落を、提供された言葉の形を変えないように注意して書いてください。\n\(words)"
-        case LangCode.es: return "Por favor escriba un párrafo que contenga las siguientes palabras, teniendo cuidado de no cambiar la forma de las palabras proporcionadas.\n\(words)"
-        case LangCode.ru: return "Пожалуйста, напишите абзац, содержащий следующие слова, стараясь не менять форму предоставленных слов.\n\(words)"
-        case LangCode.ko: return "제공된 단어의 형태가 변경되지 않도록 주의하면서 다음 단어를 포함하는 단락을 작성하십시오.\n\(words)"
-        case LangCode.de: return "Bitte schreiben Sie einen Absatz mit den folgenden Wörtern und achten Sie darauf, die Form der angegebenen Wörter nicht zu ändern.\n\(words)"
+        case LangCode.zh: return "请使用以下单词列表中的单词编写一个流畅自然的段落。注意：（1）不要改变所提供单词的形式；（2）如果使用全部单词难以编写一个流畅自然的段落，你可以选择部分单词编写，但至少选择一个。单词列表如下：\(words)"
+        case LangCode.en: return "Please write a paragraph that flows naturally using words from the word list below. Note: (1) Do not change the form of the words provided; (2) If it is difficult to write a smooth and natural paragraph using all the words, you can choose some words to write, but choose at least one. The word list is as follows: \(words)"
+        case LangCode.ja: return "以下の単語リストの単語を使用して、自然に流れる段落を書いてください。注意: (1) 提供された単語の形式を変更しないでください; (2) すべての単語を使用して滑らかで自然な段落を書くことが難しい場合は、いくつかの単語を選択して書くことができますが、少なくとも1つは選択してください。単語リストは次のとおりです。\(words)"
+        case LangCode.es: return "Por favor escriba un párrafo que fluya naturalmente usando palabras de la lista de palabras a continuación. Nota: (1) No cambie la forma de las palabras proporcionadas; (2) Si le resulta difícil escribir un párrafo fluido y natural utilizando todas las palabras, puede elegir algunas palabras para escribir, pero elija al menos una. La lista de palabras es la siguiente: \(words)"
+        case LangCode.ru: return "Пожалуйста, напишите абзац, который будет естественным, используя слова из списка слов ниже. Примечание: (1) Не меняйте форму предоставленных слов; (2) Если сложно написать плавный и естественный абзац, используя все слова, вы можете выбрать несколько слов для написания, но выберите хотя бы одно. Список слов следующий: \(words)"
+        case LangCode.ko: return "아래 단어 목록의 단어를 사용하여 자연스럽게 흐르는 문단을 작성해주세요. 참고: (1) 제공된 단어의 형태를 변경하지 마십시오; (2) 모든 단어를 사용하여 매끄럽고 자연스러운 문단을 작성하기 어려운 경우, 작성할 단어를 몇 개 선택하되 적어도 하나를 선택하십시오. 단어 목록은 다음과 같습니다. \(words)"
+        case LangCode.de: return "Bitte schreiben Sie einen Absatz, der natürlich fließt, und verwenden Sie dabei Wörter aus der folgenden Wortliste. Hinweis: (1) Ändern Sie nicht die Form der bereitgestellten Wörter. (2) Wenn es schwierig ist, einen reibungslosen und natürlichen Absatz mit allen Wörtern zu schreiben, können Sie einige Wörter zum Schreiben auswählen, aber wählen Sie mindestens eines aus. Die Wortliste lautet wie folgt: \(words)"
         default: return ""
         }
     }
