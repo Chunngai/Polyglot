1119053977129a922027826f400f33fb4567fedd
Chunngai <mikumikku3939@icloud.com>
2024-02-17 11:54:41 +0700

    reorganize files

--- Polyglot/Polyglot/Models/WordPractice.swift
+++ Polyglot/Polyglot/Models/WordPractice.swift
@@ -8,6 +8,18 @@
 
 import Foundation
 
+protocol Practice: Codable {
+    // https://stackoverflow.com/questions/50346052/protocol-extending-encodable-or-codable-does-not-conform-to-it
+    // https://developer.apple.com/documentation/foundation/archives_and_serialization/encoding_and_decoding_custom_types
+    // Due to the Practice protocol, manual coding for the practices is needed.
+}
+
+enum PracticeDirection: UInt, Codable {
+    case textToMeaning = 0
+    case meaningToText = 1
+    case text = 2
+}
+
 struct WordPractice: Practice {
     
     var id: String
