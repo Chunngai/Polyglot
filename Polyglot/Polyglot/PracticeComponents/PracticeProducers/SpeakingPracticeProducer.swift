2966f45cb65ee14f946d23812d44eba09b0b45d6
Chunngai Ho <54092982+Chunngai@users.noreply.github.com>
2025-02-06 14:33:43 +0800

    Update SpeakingPracticeProducer.swift

--- Polyglot/Polyglot/PracticeComponents/PracticeProducers/SpeakingPracticeProducer.swift
+++ Polyglot/Polyglot/PracticeComponents/PracticeProducers/SpeakingPracticeProducer.swift
@@ -13,7 +13,11 @@ class SpeakingPracticeProducer: TextMeaningPracticeProducer {
     // MARK: - Init
     
     override init(words: [Word], articles: [Article]) {
-        super.init(words: words, articles: articles)
+        super.init(
+            words: words, 
+            articles: articles, 
+            isDuolingoOnly: LangCode.currentLanguage.isDuolingoOnlyForSpeaking
+        )
         
         // Override the batch size.
         self.batchSize = LangCode.currentLanguage.configs.speakingPracticeDuration
